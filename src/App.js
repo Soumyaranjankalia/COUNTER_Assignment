a2787ff554c1eccc0031626cf9623f0b8049b6cd
Soumya <a2soumya.ranjan@gmail.com>
2022-04-08 21:33:33 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+
+import {useState} from"react"
 import './App.css';
 
 function App() {
+  const [counter,setCounter] = useState(0)
+
+  const count =(num)=>{
+      setCounter(counter+ num)
+  }
+
+  const getdouble=(number)=>{
+      setCounter(counter * number)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h2 className={counter%2 === 0 ? "greenclr":"redclr"}>Counter:{counter}</h2>
+     <button onClick={()=>count(+1)}>Increment (+)</button>
+
+    <button onClick={()=>count(-1)}>Decrement (-)</button>
+
+    <button onClick={()=>getdouble(2)}>Double</button>
     </div>
   );
+
+
 }
 
 export default App;
